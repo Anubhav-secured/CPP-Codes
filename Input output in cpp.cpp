cbe2b7570ac82fecfbb1759008740ada11f84004
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2021-08-04 17:12:32 +0530

    Update and rename tut4.cpp to Input output in cpp.cpp

--- Input output in cpp.cpp
+++ Input output in cpp.cpp
@@ -7,7 +7,7 @@ int main()
     int b=15;
     float pi=3.14;
     char c='U';
-    cout<<"this is tutorial 4.\nhere value of a is: "<<a<<"\nvalue of b is: "<<b;
+    cout<<"here value of a is: "<<a<<"\nvalue of b is: "<<b;
     cout<<"\nthe value of pi is: "<<pi;
     cout<<"\nthe value of c is: "<<c;
     return(0);
